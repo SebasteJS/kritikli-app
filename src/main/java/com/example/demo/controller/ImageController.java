a888326b5a092be71fa816b033433cf05b3763a1
SebasteJS <sebastian.ksiel.js@gmail.com>
2021-01-14 14:43:20 +0100

    renamed template add controllers

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main/image")
+@RequiredArgsConstructor
 public class ImageController {
+
 }
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,4 +1,12 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+@RequiredArgsConstructor
 public class MainController {
+
 }
